ed29d43c727943f4a8e9c5baa8fec85089166223
Maria <maria.brunned@gmail.com>
2023-10-02 15:52:13 +0200

    Simple behavior tree works

--- Assets/Scripts/BehaviourTree/Tree.cs
+++ Assets/Scripts/BehaviourTree/Tree.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace BehaviourTree
@@ -8,18 +10,70 @@ namespace BehaviourTree
     {
         private Node _root = null;
 
+        public UnityEngine.GameObject gameObjectEnemy;
+        public UnityEngine.GameObject gameObjectPlayer1;
+        public UnityEngine.GameObject gameObjectPlayer2;
+        public UnityEngine.GameObject gameObjectPlayer3;
+        //public UnityEngine.GameObject gameObjectBattleSystem;
+
+        protected Unit enemy;
+        protected Unit player1;
+        protected Unit player2;
+        protected Unit player3;
+        //private BattleSystem battleSystem;
+        private BattleState currentState;
+
+        public Boolean unitIsDead = false;
+
+        private Boolean oneTime = false;
+
         protected void Start ()
         {
+            enemy = gameObjectEnemy.GetComponent<Unit>();
+            player1 = gameObjectPlayer1.GetComponent<Unit>();
+            player2 = gameObjectPlayer2.GetComponent<Unit>();
+            player3 = gameObjectPlayer3.GetComponent<Unit>();
+            //battleSystem = gameObjectBattleSystem.GetComponent<BattleSystem>();
+            currentState = GameObject.FindObjectOfType<BattleSystem>().state;
+            Debug.Log("BattleState: " + currentState);
+
             _root = SetupTree();
         }
 
-        private void Update()
+        // Få funktionen att bara köra en gång när vi vill att den ska köras en gång 
+        public void FixedUpdate()
         {
-            if (_root != null)
+            int doOnce = PlayerPrefs.GetInt("behaviorTreeDoOnce");
+            currentState = GameObject.FindObjectOfType<BattleSystem>().state;
+            if (currentState == BattleState.ENEMYTURN && doOnce == 1)
             {
-                _root.Evaluate();
+                // To make the behavior tree run once
+                doOnce = 2;
+                PlayerPrefs.SetInt("behaviorTreeDoOnce", doOnce);
+
+                Debug.Log("Tree node created");
+                if (_root != null)
+                {
+                    Debug.Log("Root is noy null");
+                    _root.Evaluate();
+
+                }
             }
         }
+
+        /*public void GetEvaluate()
+        {
+            currentState = GameObject.FindObjectOfType<BattleSystem>().state;
+            if (currentState == BattleState.ENEMYTURN)
+            {
+                Debug.Log("Tree node created");
+                if (_root != null)
+                {
+                    _root.Evaluate();
+                }
+            }
+
+        }*/
 
         protected abstract Node SetupTree();
     }
